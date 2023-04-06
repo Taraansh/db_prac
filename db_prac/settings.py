ef4a0f8894cd1e8ed1f0b7cc65dfda9ad56a8a65
Taraansh <taraanshnandwani123@gmail.com>
2023-04-06 21:58:20 +0530

    ...

--- db_prac/settings.py
+++ db_prac/settings.py
@@ -149,7 +149,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
